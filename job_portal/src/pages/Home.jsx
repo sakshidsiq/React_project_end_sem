2cabfa43cc39c2a983dbedc8448737a66f65d209
sakshidsiq <sakshi.thakkar@mediaamp.co.in>
2025-03-25 01:01:27 +0530

    iplemented clerk authentication of login, added hero components to home page and added search bar functionality to the app

--- job_portal/src/pages/Home.jsx
+++ job_portal/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Joblisting from '../components/Joblisting'
 
 const Home = () => {
   return (
     <div>
     <Navbar />
+    <Hero />
+    <Joblisting />
     </div>
   )
 }
